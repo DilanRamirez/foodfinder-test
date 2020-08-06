74603c6470824d559ac6064e194b558ad8b88ea4
DilanRamirez <drramirezra@miners.utep.edu>
2020-08-06 13:25:15 -0700

    new card view

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var map = L.map("mapid").setView([19.03793, -98.20346], 14);
+var map = L.map("mapid").setView([19.03793, -98.20346], 12);
 
 L.tileLayer(
   "https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=ULohX30zZKHpFLtElHY4",
@@ -26,7 +26,7 @@ const fetchData = async () => {
       long: results.records.map((item) => item.Longitud),
       actualizacion: results.records.map((item) => item.actualizacion),
     };
-    console.log(parsedData)
+    console.log(parsedData);
 
     for (var i = 0; i < parsedData.nombre.length; i++) {
       var html = [
@@ -76,12 +76,9 @@ const fetchData = async () => {
 
 const restaurantes = async () => {
   const response = await fetch("restaurantes.json");
-  console.log(response);
   if (response.ok) {
     const data = await response.json();
     const results = data.result;
-    console.log(results);
-
 
     const parsedData = {
       nombre: results.records.map((item) => item.Nombre),
@@ -92,27 +89,9 @@ const restaurantes = async () => {
       lat: results.records.map((item) => item.Latitud),
       long: results.records.map((item) => item.Longitud),
     };
-    console.log(parsedData)
+    console.log(parsedData);
 
     for (var i = 0; i < parsedData.nombre.length; i++) {
-      var html = [
-        '<div class="col-md-auto">' +
-          `<div class="card" id=${i}>` +
-          '<div class="card-body">' +
-          '<h5 class="card-title">' +
-          parsedData.nombre[i] +
-          "</h5>" +
-          '<p class="card-text">' +
-          parsedData.direccion[i] + '<br>'+
-          '<b>'+parsedData.costo[i] +'</b>'+
-          "</p>" +
-          "</div>" +
-          "</div>" +
-          "</div>",
-      ];
-
-      $("#container-restaurantes").append(html);
-
       var firefoxIcon = L.icon({
         iconUrl:
           "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-512.png",
@@ -122,28 +101,57 @@ const restaurantes = async () => {
 
       var marker = L.marker([parsedData.lat[i], parsedData.long[i]], {
         // elevation: 260.0,
-        title: "Transamerica Pyramid",
+        title: "Restaurantes",
         icon: firefoxIcon,
       }).addTo(map);
 
-      marker
-        .bindPopup(
-          `<b>${parsedData.nombre[i]}</b><br>
+      marker.bindPopup(
+        `<b>${parsedData.nombre[i]}</b><br>
            ${parsedData.direccion[i]}<br>
            ${parsedData.telefono[i]}<br>
            ${parsedData.horario[i]}<br>
            <b>${parsedData.costo[i]}</b><br>`
-          
-        )
-        .openPopup();
+      );
+
+      marker.on("click", function (e) {
+        map.setView([e.latlng.lat, e.latlng.lng], 18);
+        for (var i = 0; i < parsedData.nombre.length; i++) {
+          if (
+            Number(parsedData.lat[i]) === e.latlng.lat &&
+            Number(parsedData.long[i]) === e.latlng.lng
+          ) {
+            var html = [
+              '<h2 class="card-title">' +
+                parsedData.nombre[i] +
+                "</h2>" +
+                '<div class="card-subInfo">' +
+                "<p>" +
+                parsedData.direccion[i] +
+                "</p>" +
+                '<p class="black-bold"> <b>' +
+                parsedData.costo[i] +
+                "</b></p>" +
+                '<div class="card-stars">'+
+                '<i class="fa fa-star fa-lg"></i>'+
+                '<i class="fa fa-star fa-lg"></i>'+
+                '<i class="fa fa-star fa-lg"></i>'+
+                '</div>'
+            ];
+
+            $("#card-content").append(html);
+            map.setView([e.latlng.lat, e.latlng.lng], 12);
+          }
+        }
+        $("#container-card").show();
+      });
     }
   }
-}
-
+};
 
 $(document).ready(function () {
   $("#container-turistico").hide();
   $("#container-restaurantes").hide();
+  $("#container-card").hide();
 });
 
 $("#turisitco").click(function () {
@@ -154,4 +162,11 @@ $("#turisitco").click(function () {
 $("#restaurantes").click(function () {
   $("#container-restaurantes").toggle("show");
   restaurantes();
+  document.getElementById("cantainer-card").innerHTML = "";
+});
+
+$("#close-card").click(function () {
+  console.log("clicked");
+  $("#container-card").hide();
+  document.getElementById("card-content").innerHTML = "";
 });
